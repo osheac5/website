c08ea48e07b98c816a42ddea6ee88a46e31fe111
Conor OShea <osheac5@tcd.ie>
2018-11-08 23:51:36 +0000

    Website structure outline complete

--- aboutMe/modal.js
+++ aboutMe/modal.js
@@ -1,77 +1,15 @@
 function openModal() {
-  document.getElementById('myModal').style.display = "flex";
+  document.getElementById('myCarousel').style.display = "flex";
 }
 
 function closeModal() {
-  document.getElementById('myModal').style.display = "none";
+  document.getElementById('myCarousel').style.display = "none";
 }
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("caption-text");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].innerHTML;
-}
-
-/************************/
-/***  Modal 2  **********/
-/************************/
 
 function openModal2() {
-  document.getElementById('modal2').style.display = "flex";
+  document.getElementById('carousel2').style.display = "flex";
 }
 
 function closeModal2() {
-  document.getElementById('modal2').style.display = "none";
-}
-
-var slideIndex2 = 1;
-showSlides(slideIndex2);
-
-function plusSlides(n) {
-  showSlides(slideIndex2 += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex2 = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slides2");
-  var dots = document.getElementsByClassName("caption-text2");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex2 = 1}
-  if (n < 1) {slideIndex2 = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex2-1].style.display = "block";
-  dots[slideIndex2-1].className += " active";
-  captionText.innerHTML = dots[slideIndex2-1].innerHTML;
+  document.getElementById('carousel2').style.display = "none";
 }
